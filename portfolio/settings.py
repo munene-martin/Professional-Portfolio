47a2a36b2646a60edcad25a498daa9147e5388e4
munene-martin <martinmunene@tutamail.com>
2026-01-05 13:38:25 +0300

    Update settings.py
    Added Allowed Hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--5b01o9!0mxj$mu_i)3b8$z!-!5qv!rqv4g8-hufgci2p-zaq7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
