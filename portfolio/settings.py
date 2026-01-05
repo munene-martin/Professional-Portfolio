396955a19fd2ed8375b3d8f2f00b0abd703c5a30
munene-martin <martinmunene@tutamail.com>
2026-01-05 13:41:38 +0300

    Update settings.py
    Debug turned to false in production Mode

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--5b01o9!0mxj$mu_i)3b8$z!-!5qv!rqv4g8-hufgci2p-zaq7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
